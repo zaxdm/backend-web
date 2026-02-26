ccb7399f2afdb8e7977f52cdd73fb471c7764212
zaxdm <esteban.dioses@tecsup.edu.pe>
2026-02-26 18:00:19 -0500

    cambios

--- src/routes/api/UsuarioRoutes.js
+++ src/routes/api/UsuarioRoutes.js
@@ -2,18 +2,13 @@ const express = require('express');
 const router = express.Router();
 const usuarioController = require('../../controllers/UsuarioController');
 
-// ------------------ PERFIL ------------------
-// Esto debe ir antes de '/:id' para que no choque
 router.get('/perfil', usuarioController.obtenerPerfil);
 
-// ------------------ USUARIOS ------------------
 router.get('/', usuarioController.obtenerUsuarios);
 router.get('/:id', usuarioController.obtenerUsuarioPorId);
 
-// ------------------ REGISTRO PÚBLICO ------------------
 router.post('/', usuarioController.registrarUsuario);
 
-// ------------------ ACTUALIZAR Y ELIMINAR ------------------
 router.put('/:id', usuarioController.actualizarUsuario);
 router.delete('/:id', usuarioController.eliminarUsuario);
 
