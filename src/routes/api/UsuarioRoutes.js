ec3f60a6fc0b5f38d0e7a8e5ce53b185d2d69a9a
zaxdm <esteban.dioses@tecsup.edu.pe>
2026-02-26 17:42:16 -0500

    ----

--- src/routes/api/UsuarioRoutes.js
+++ src/routes/api/UsuarioRoutes.js
@@ -2,15 +2,12 @@ const express = require('express');
 const router = express.Router();
 const usuarioController = require('../../controllers/UsuarioController');
 
-// Registro público
 router.post('/register', usuarioController.registrarUsuario);
 
-// Resto de rutas protegidas
 router.get('/', usuarioController.obtenerUsuarios);
 router.get('/:id', usuarioController.obtenerUsuarioPorId);
+router.post('/', usuarioController.crearUsuario);
 router.put('/:id', usuarioController.actualizarUsuario);
 router.delete('/:id', usuarioController.eliminarUsuario);
-
-router.get('/perfil', usuarioController.obtenerPerfil);
 
 module.exports = router;
