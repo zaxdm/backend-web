da2370d69bd4ac15a20a0dba5aef7dc20fddf85a
zaxdm <esteban.dioses@tecsup.edu.pe>
2026-02-27 15:13:19 -0500

    cambios-uploadcards

--- src/config/db.js
+++ src/config/db.js
@@ -7,7 +7,11 @@ const pool = mysql.createPool({
     port: process.env.DB_PORT || 3306, 
     user: process.env.DB_USER, 
     password: process.env.DB_PASSWORD, 
-    database: process.env.DB_NAME, 
+    database: process.env.DB_NAME,
+    // Agregar esto:
+    connectionLimit: 4,
+    waitForConnections: true,
+    queueLimit: 0
 });
 
 
