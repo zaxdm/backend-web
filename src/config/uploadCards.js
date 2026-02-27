2a0bd34b465578445df83efa76f59f6d090c076d
zaxdm <esteban.dioses@tecsup.edu.pe>
2026-02-27 15:03:45 -0500

    cambios.env

--- src/config/uploadCards.js
+++ src/config/uploadCards.js
@@ -1,6 +1,6 @@
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('../config/cloudinary');
+const cloudinary = require('./cloudinary');
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
