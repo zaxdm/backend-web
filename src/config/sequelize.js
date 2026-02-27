9402eaff0dbdcefe2c041459f03c2b74abcdc6fd
zaxdm <esteban.dioses@tecsup.edu.pe>
2026-02-27 15:20:10 -0500

    cambios-config

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -17,17 +17,9 @@ const sequelize = new Sequelize(
             min: 0,
             acquire: 30000,
             idle: 10000
-        }
+        },
+        logging: false
     }
 );
-
-
-sequelize.authenticate()
-    .then(() => {
-        console.log('Conexión a la base de datos establecida con éxito');
-    })
-    .catch((err) => {
-        console.error('No se pudo conectar a la base de datos:', err);
-    });
 
 module.exports = sequelize;
